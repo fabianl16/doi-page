6497d7aba68179084b9aa4438b234bf88a157c1e
Fabian Loaeza <fabianloaeza8@gmail.com>
2023-11-21 00:33:02 -0700

    Ajuste fade de entrada

--- src/app/home/animations/fade-animations.ts
+++ src/app/home/animations/fade-animations.ts
@@ -3,9 +3,9 @@ import { trigger, transition, style, animate } from '@angular/animations';
 export const fadeAnimation = trigger('fadeAnimation', [
   transition(':enter', [
     style({ opacity: 0 }),
-    animate('2000ms', style({ opacity: 1 })),
+    animate('1000ms', style({ opacity: 1 })),
   ]),
   transition(':leave', [
-    animate('2000ms', style({ opacity: 0 })),
+    animate('1000ms', style({ opacity: 0 })),
   ]),
 ]);
